d42ee205835f87f9266dc20b0217c6e56423c567
“Brett <bretttewell@gmail.com>
2019-02-21 21:49:06 -0600

    HTTP_requests - updating word definition app with user function

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -7,10 +7,15 @@ query = gets.chomp
 response = HTTP.get("https://api.wordnik.com/v4/word.json/#{query}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")  
 
 word_definition = response.parse
-word = word_definition[0]["word"]
-definition_one = word_definition[0]["text"]
-definition_two = word_definition[1]["text"]
+i = 0
+word_definition.length.times do     
+  p "#{i + 1}. #{word_definition[i]["text"]}"
+  i+=1
+end
+# word = word_definition[0]["word"]
+# definition_one = word_definition[0]["text"]
+# definition_two = word_definition[1]["text"]
 
-p word
-p definition_one
-p definition_two
+# p word
+# p definition_one
+# p definition_two
